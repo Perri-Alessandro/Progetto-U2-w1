9784c7e89d39279961721c8e93ef2efe6e25a86f
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2023-12-23 15:16:07 +0100

    ok, manca animazione M

--- base_di_partenza/scripts/script.js
+++ base_di_partenza/scripts/script.js
@@ -19,7 +19,25 @@ m.innerText = `M M M M M M M M M M M M M M
 const divHeaderM = document.querySelector(".headerDxM");
 divHeaderM.appendChild(m);
 
-// CREO ARRAY PER ANIMAZIONE
+// CREO ARRAY PER ANIMAZIONE M
 const testo = m.innerText.replace(/\s+/g, ""); // Ottiengo il testo senza spazi e a capo
 const arrayM = testo.split(""); // Converto il testo in un array di singoli caratteri
 console.log("Creato array per animazione M", arrayM);
+
+// Animazine colore navBar a scroll
+window.addEventListener("scroll", function () {
+  const navFixed = document.querySelector(".nav");
+  const navButton = document.getElementById("buttonNav");
+  const headerBottom = document.querySelector(".headerBottom");
+
+  const navFixedPositionEsize = navFixed.getBoundingClientRect().bottom;
+  const headerBottomPositionEsize = headerBottom.getBoundingClientRect().bottom;
+
+  if (navFixedPositionEsize >= headerBottomPositionEsize) {
+    navFixed.classList.add("scrolled");
+    navButton.classList.add("scrolled");
+  } else {
+    navFixed.classList.remove("scrolled");
+    navButton.classList.remove("scrolled");
+  }
+});
